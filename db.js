137060f2c58a97d268a2fe6520abef73269ccd8f
am5350498-hash <am5350498@gmail.com>
2026-08-19 20:54:00 +0530

    Update MySQL connection details for production

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'hospiatl_web'
+    host:'bcubl5pqh0if7ygqpnmx-mysql.services.clever-cloud.com',
+    user:'uqjpnnfxzjee8iap',
+    password:'3o6YId7ebPIpNDNO0ca1',
+    database:'bcubl5pqh0if7ygqpnmx'
 })
 
 var exe=util.promisify(conn.query).bind(conn);
